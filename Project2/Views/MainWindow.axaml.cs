e6401fb8dbbc5c4cecb40bde8fa5687194f37b78
AraunHaracti <AraunHaracti@gmail.com>
2023-10-26 20:51:40 +0700

    added database connector class

--- Project2/Views/MainWindow.axaml.cs
+++ Project2/Views/MainWindow.axaml.cs
@@ -7,5 +7,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        Database.ConnectionStringBuilder = new()
+        {
+            Server = "10.10.1.24",
+            Port = 3306,
+            Database = "pro1_12",
+            UserID = "user_01",
+            Password = "user01pro"
+        };
     }
 }
